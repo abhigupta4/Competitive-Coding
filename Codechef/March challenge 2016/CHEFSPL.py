c9b9a8fa6c3f9fdbae599aa9b70f36739b338ea5
Abhinav Gupta <abhinavgupta6245@gmail.com>
2016-03-22 13:01:45 +0530

    March16

--- Codechef/March challenge 2016/CHEFSPL.py
+++ Codechef/March challenge 2016/CHEFSPL.py
@@ -11,97 +11,32 @@ def chef_spl(string1):
 		half = len1 / 2
 		if string1[1:half + 1] == string1[half + 1:]:
 			return "YES"
+
+		#Element to be deleted is on left half
 		count = 0
-		del1 = 0
 		left_shift = 0
-		right_shift = 0
 		while (count < half):
-			if (count + 1 + right_shift + half) < len1:
-				if string1[count + left_shift] == string1[count + 1 + right_shift + half]:
-					count += 1
-					continue
-				else:
-					pass
-			if string1[count + 1] == string1[count + 1 + half]:
-				if del1 > 0:
-					break
-				if (count + 2 + half) < len1 :
-					if string1[count + 2] == string1[count + 2 + half]:
-						left_shift = 1
-						del1 = 1
-						count += 1
-						continue
-					else:
-						pass
-				else:
-					count += 1
-					break
-			if (count + 2 + half) < len1:
-				if string1[count] == string1[count + 2 + half]:
-					if del1 > 0:
-						break
-					if (count + 3 + half) < len1:
-						if string1[count + 1] == string1[count + 3 + half]:
-							right_shift = 1
-							del1 = 1
-							count += 1
-							continue
-						else:
-							break
-					else:
-						count += 1
-						break
-				else:
-					break
+			if string1[count + left_shift] == string1[count + half + 1]:
+				count += 1
+			elif string1[count + 1] == string1[count + half + 1]:
+				left_shift = 1
+				count += 1
 			else:
 				break
 
 		if count == half:
 			return "YES"
 
+		#Element to be deleted is on right half
+
 		count = 0
-		del1 = 0
-		left_shift = 0
 		right_shift = 0
 		while (count < half):
-			if (count + right_shift + half) < len1:
-				if string1[count + left_shift] == string1[count + right_shift + half]:
-					count += 1
-					continue
-				else:
-					pass
-			if string1[count + 1] == string1[count + half]:
-				if del1 > 0:
-					break
-				if (count + 1 + half) < len1:
-					if string1[count + 2] == string1[count + 1 + half]:
-						left_shift = 1
-						del1 = 1
-						count += 1
-						continue
-					else:
-						pass
-				else:
-					count += 1
-					break
-				
-			if (count + 1 + half) < len1:
-				if string1[count] == string1[count + 1 + half]:
-					if del1 > 0:
-						break
-					if (count + 2 + half) < len1:
-						if string1[count + 1] == string1[count + 2 + half]:
-							right_shift = 1
-							del1 = 1
-							count += 1
-							continue
-						else:
-							break
-					else:
-						count += 1
-						break
-				else:
-					break
+			if string1[count] == string1[count + half + right_shift]:
+				count += 1
+			elif string1[count] == string1[count + half + 1]:
+				right_shift = 1
+				count += 1
 			else:
 				break
 
@@ -110,7 +45,8 @@ def chef_spl(string1):
 
 		return "NO"
 
-
 for _ in range(input()):
 	string1 = raw_input()
 	print chef_spl(string1)
+
+#AC
--- Codechef/March challenge 2016/seatstr2_try.py
+++
@@ -1,4 +0,0 @@
-mod = 10**9 + 7
-temp = 1
-for i in range(1,100000):
-    temp = (temp * (i % mod))%mod
